fc445b21b37b86cf658a2c4bc846d162ff4c623c
Aryan kumar <aryhu@Aryans-MacBook-Air-8.local>
2026-03-23 18:06:29 +0530

    adding data

---
+++ src/data/user.js
@@ -0,0 +1,75 @@
+const INFO = {
+	main: {
+		title: "Aryan's Portfolio",
+		name: "Aryan kumar",
+		email: "aryankr567890@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://x.com/aryankr4120",
+		github: "https://github.com/ak75494459",
+		linkedin: "https://www.linkedin.com/in/aryan-kumar-40306b283/",
+		instagram: "https://www.instagram.com/aryankr7549/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		description:
+			"I am a full-stack developer skilled in Node.js, Express.js, MongoDB, React.js, Next.js, and React Native (Expo). I build scalable web and mobile applications with REST APIs, authentication systems like JWT and Auth0, and modern tools such as Tailwind CSS, ShadCN UI, Zod, and Convex. I also have a strong foundation in C++, data structures, and algorithms, and I use Git and GitHub for version control.",
+	},
+
+	about: {
+		title: "I’m Aryan kumar. I live in Punjab, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"A full-stack room booking application that allows users to explore listings, search rooms, and make bookings seamlessly. Built with a React frontend and a Node.js, Express, and MongoDB backend, it features secure authentication, RESTful APIs, and a responsive interface for a smooth and reliable booking experience.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/ak75494459/LGRooms_Frontend",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				" full-stack food ordering application that allows users to browse menus, place orders, and manage their cart seamlessly. Built with a React frontend and a Node.js, Express, and MongoDB backend, the app features secure authentication, RESTful APIs, and a responsive UI, ensuring a smooth and efficient user experience.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/ak75494459/food-ordering-app-frontend",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"A Neo Colab (Examly/LPU coding platform) clone where students can practice coding problems, attempt assessments, and view results in an interactive environment",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/ak75494459/colab",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"“A full-stack social media mobile application where users can create posts, like, and comment in real-time, built using React Native (Expo), Convex backend, and Clerk authentication",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://expo.dev/accounts/aryankr5678",
+		},
+	],
+};
+
+export default INFO;
